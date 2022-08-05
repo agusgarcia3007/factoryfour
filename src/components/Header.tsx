69229dc7a7be2f87b31a8fcc0987ec6dde07dcf2
Mobile <agustin@getcharly.com>
2022-08-04 23:12:32 -0300

    created header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1 className="header_title">FactoryFour status dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
